4516f0856b614f9a07a90b9aeca51ff61b76c262
Scott Doggett <sdoggett@lcs.on.ca>
2020-05-29 14:51:33 -0400

    Assignment View created

--- Culminating - Assignment Tracker/ViewController.swift
+++ Culminating - Assignment Tracker/ViewController.swift
@@ -13,8 +13,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+    }    
+    
+    
+    
 }
 
